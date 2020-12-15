16ea699c553be67e59f1386fe69dbafb3ba148e8
donnysim <donatas.smk@gmail.com>
2020-12-15 13:06:06 +0200

    feat: add ValidatorFactory prep.

--- src/ValidatorFactory.php
+++ src/ValidatorFactory.php
@@ -4,7 +4,19 @@ declare(strict_types=1);
 
 namespace DonnySim\Validation;
 
+use DonnySim\Validation\Contracts\MessageResolver;
+
 class ValidatorFactory
 {
+    protected MessageResolver $resolver;
 
+    public function __construct(MessageResolver $resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
+    public function make(array $data, array $rules, array $attributeNames = []): Validator
+    {
+        return new Validator($this->resolver, $data, $rules, $attributeNames);
+    }
 }
