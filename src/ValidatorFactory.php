a4dbf2ac29cc66a1b2d70dd90c3a67dc786792f4
donnysim <donatas.smk@gmail.com>
2020-12-15 13:17:14 +0200

    feat: introduce singleton pattern for ValidatorFactory.

--- src/ValidatorFactory.php
+++ src/ValidatorFactory.php
@@ -5,14 +5,31 @@ declare(strict_types=1);
 namespace DonnySim\Validation;
 
 use DonnySim\Validation\Contracts\MessageResolver;
+use UnexpectedValueException;
 
 class ValidatorFactory
 {
+    protected static ?self $instance = null;
+
     protected MessageResolver $resolver;
 
     public function __construct(MessageResolver $resolver)
     {
         $this->resolver = $resolver;
+    }
+
+    public static function setInstance(self $instance): void
+    {
+        static::$instance = $instance;
+    }
+
+    public static function instance(): self
+    {
+        if (!static::$instance) {
+            throw new UnexpectedValueException('Global instance has not been set.');
+        }
+
+        return static::$instance;
     }
 
     public function make(array $data, array $rules, array $attributeNames = []): Validator
