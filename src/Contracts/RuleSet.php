dcb2971c76fda1d73b1e0a295904bc0c12a3a5f9
donnysim <donatas.smk@gmail.com>
2021-03-07 15:41:27 +0200

    chore: add docs to RuleSet.

--- src/Contracts/RuleSet.php
+++ src/Contracts/RuleSet.php
@@ -6,7 +6,17 @@ namespace DonnySim\Validation\Contracts;
 
 interface RuleSet
 {
+    /**
+     * Get attribute pattern for validation.
+     *
+     * @return string
+     */
     public function getPattern(): string;
 
+    /**
+     * Get rules.
+     *
+     * @return \DonnySim\Validation\Contracts\Rule[]
+     */
     public function getRules(): array;
 }
