f5a87bd8f56d279f7dfad829cf71214eca24cefc
donnysim <donatas.smk@gmail.com>
2021-03-08 20:45:26 +0200

    chore: tweak dependencies.

--- src/RuleGroup.php
+++ src/RuleGroup.php
@@ -9,12 +9,12 @@ use DonnySim\Validation\Contracts\RuleGroup as RuleGroupContract;
 class RuleGroup implements RuleGroupContract
 {
     /**
-     * @var array|\DonnySim\Validation\Contracts\RuleSet[]
+     * @var array|\DonnySim\Validation\Contracts\RuleSet[]|\DonnySim\Validation\Contracts\RuleGroup[]
      */
     protected array $rules = [];
 
     /**
-     * @param \DonnySim\Validation\Contracts\RuleSet[] $rules
+     * @param \DonnySim\Validation\Contracts\RuleSet[]|\DonnySim\Validation\Contracts\RuleGroup[] $rules
      */
     public function __construct(array $rules)
     {
