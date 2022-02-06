a1404ef056aeb9bf546db5596aed75ab4ee75a0c
donnysim <donatas.smk@gmail.com>
2022-02-06 21:07:55 +0200

    fix: add missing interface declaration.

--- src/Interfaces/RuleInterface.php
+++ src/Interfaces/RuleInterface.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace DonnySim\Validation\Interfaces;
 
+use DonnySim\Validation\Data\DataEntry;
+use DonnySim\Validation\Process\EntryProcess;
+
 interface RuleInterface
 {
-
+    public function validate(DataEntry $entry, EntryProcess $process): void;
 }
